4690f54091c6562c7ca6ae39f3f28d8e0d7cb9cb
Dani0604 <37442044+Dani0604@users.noreply.github.com>
2018-05-04 22:41:40 +0200

    Ujra

--- TankGame/src/TankGame/Main.java
+++ TankGame/src/TankGame/Main.java
@@ -17,6 +17,7 @@ public class Main {
 	 * @param args
 	 *            the command line arguments
 	 */
+
 	public static void main(String[] args) {
 		StateMachine SM = new StateMachine();
 		GUI g = new GUI(SM);
